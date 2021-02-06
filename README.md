fc19e7b0fd241204c65a7bea36cd3b22835707c1
ParkKiyoung <ky91zzang@naver.com>
2021-02-06 17:13:24 +0900

    basic Form comp

--- README.md
+++ README.md
@@ -12,4 +12,5 @@
 
 - 업로드 페이지 생성
 - navBar에 업로드 페이지 버튼 생성
+- select 이벤트는 일반 자바스크립트랑 구조가 다를게 없음. onChange 이벤트 걸어주고 select State 교체
 -
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Typography,Button,Form,Input} from 'antd';
+
+const {Title} = Typography;
+const {TextArea} = Input;
+const Continents=[
+    {key:1, value:"Africa"},
+    {key:2, value:"Europe"},
+    {key:3, value:"Asia"},
+    {key:4, value:"North America"},
+    {key:5, value:"South America"},
+    {key:6, value:"Austrailia"},
+    {key:7, value:"Antarctica"}
+]
 
 function UploadProductPage() {
+    const [Name, setName] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Price, setPrice] = useState(0);
+    const [Continent, setContinent] = useState(1);
+    const [Image, setImage] = useState([]);
+
+
+    const TitileHandler=(e)=>{
+        setName(e.currentTarget.value);
+    }
+    const DescriptionHandler=(e)=>{
+        setDescription(e.currentTarget.value);
+    }
+    const PriceHandler=(e)=>{
+        setPrice(e.currentTarget.value);
+    }
+    const ContinentHandler =(e)=>{
+        setContinent(e.currentTarget.value)
+    }
+
+
     return (
-        <div>
-            UploadProductPage
+        <div style={{maxWidth:'700px' , margin:'2rem auto'}}>
+            <div style={{textAlign:'center', marginBottom:'2rem'}}>
+                <Title>여행 상품 업로드</Title>
+            </div>
+            <Form>
+
+                <br/>
+                <br/>
+                <label>이름</label>
+                <Input  value={Name} onChange={TitileHandler}/>
+                <br/>
+                <br/>
+                <label>설명</label>
+                <TextArea value={Description} onChange={DescriptionHandler}/>
+                <br/>
+                <br/>
+                <label>가격($)</label>
+                <Input type="number" value={Price} onChange={PriceHandler}/>
+                <br/>
+                <br/>
+                <select onChange={ContinentHandler} value={Continent}>
+                    {Continents.map((item,i)=>(
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                    ))}
+                </select >
+                <br/>
+                <br/>
+                <Button>
+                    확인
+                </Button>
+
+            </Form>
+            
         </div>
     )
 }
